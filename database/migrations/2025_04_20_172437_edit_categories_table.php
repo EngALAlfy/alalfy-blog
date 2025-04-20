b16463fe5c1756797440498bbc08d0fbe1f08bdf
Islam H ALAlfy <islam@alalfy.com>
2025-04-20 21:10:27 +0200

    Remove unique constraint from slug columns in migrations.
    The `slug` columns in `categories` and `posts` tables no longer enforce uniqueness while retaining indexing. This change allows for greater flexibility in handling slugs without compromising performance.

--- database/migrations/2025_04_20_172437_edit_categories_table.php
+++ database/migrations/2025_04_20_172437_edit_categories_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string("slug" , 300)->unique()->index()->after("name");
+            $table->string("slug" , 300)->index()->after("name");
         });
     }
 
